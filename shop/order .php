ef90e98ed2d6759caf29df0e98c9c256b2dccd0e
lim8639 <863978160@qq.com>
2020-12-15 20:01:00 +0800

    1010/12/15 15:30 本地更新

--- shop/order .php
+++ shop/order .php
@@ -14,23 +14,14 @@
     </style>
 </head>
 <body>
-<header>
-
-</header>
+<?php  include "../utils/header.php"?>
 <div class="container">
 
     <div class="row">
 
     </div>
 </div>
-<footer>
-    <div class="container">
-        <div class="copyright">
-            <p> @2018-2021 水果新语</p>
-            <p>©copyright 版权所有侵权必究</p>
-            <p>@0771-10086 @email 863978160@qq.com</p>
-        </div>
-    </div>
-</footer>
+<?php  include "../utils/footer.php"?>
+
 </body>
 </html>
--- utils/footer.php
+++ utils/footer.php
@@ -1,7 +1,6 @@
 <!--页脚-->
 
-<footer style="width: 100%">
-
+<footer >
         <div class="copyright">
             <p> @2018-2021 水果新语</p>
             <p>©copyright 版权所有侵权必究</p>
